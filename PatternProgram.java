0de090f52b30416d7517c9c33651d67f09070a79
Nikhitha-vadla <117642036+Nikhitha-vadla@users.noreply.github.com>
2022-11-13 21:49:59 +0530

    Add files via upload

---
+++ PatternProgram.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class PatternProgram {
+	public static void main(String args[]) {
+		int i, j, n;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the no of lines");
+		n = sc.nextInt();
+		for (i = 1; i <= n; i++) {
+			if (i != 1) {
+				System.out.printf("  ");
+			}
+			for (j = 1; j <= n - i; j++) {
+				System.out.printf("  ");
+			}
+			for (j = 1; j <= i; j++) {
+				System.out.printf(" %d", j);
+			}
+			for (j = i - 1; j >= 1; j--) {
+				System.out.printf(" %d", j);
+			}
+
+			System.out.println();
+		}
+	}
+}
